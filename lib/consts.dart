8267332b2e59902a3e06402272b701d831d7859a
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-05-21 01:35:47 -0300

    card saldo created

---
+++ lib/consts.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const whiteColor = Color(0xFFFFFFFF);
+const lightPink = Color(0xFFFAA4A5);
+const dartPink = Color(0xFFF77171);
--- lib/pages/finance_page.dart
+++ lib/pages/finance_page.dart
@@ -1,3 +1,4 @@
+import 'package:finance/consts.dart';
 import 'package:flutter/material.dart';
 
 class FinancePage extends StatefulWidget {
@@ -19,6 +20,50 @@ class FinancePageState extends State<FinancePage> {
           ),
         ),
         backgroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 10),
+            Container (
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: 170,
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                  lightPink,
+                  dartPink,
+                ]),
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              padding: const EdgeInsets.all(16),
+              alignment: Alignment.center,
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                   Text('Saldo',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color:  Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
